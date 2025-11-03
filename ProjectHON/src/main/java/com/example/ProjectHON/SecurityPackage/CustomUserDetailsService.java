e6874a4d9689cc556ea1818a18f3832624709103
Muskan Shroti <shrotimuskan@gmail.com>
2025-11-03 23:53:29 +0530

    Refactor CustomUserDetailsService to use username

--- ProjectHON/src/main/java/com/example/ProjectHON/SecurityPackage/CustomUserDetailsService.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/SecurityPackage/CustomUserDetailsService.java
@@ -1,28 +1,26 @@
-//package com.example.ProjectHON.SecurityPackage;
-//
-//import com.example.ProjectHON.User_masterpackage.UserMaster;
-//import com.example.ProjectHON.User_masterpackage.UserMasterRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class CustomUserDetailsService implements UserDetailsService {
-//
-//    @Autowired
-//    private UserMasterRepository userMasterRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        UserMaster user = userMasterRepository.findByEmail(email)
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
-//
-//        return org.springframework.security.core.userdetails.User
-//                .withUsername(user.getEmail())
-//                .password(user.getPassword())
-//                .roles("USER") // you can also use user.getRole() if you have one
-//                .build();
-//    }
-//}
+package com.example.ProjectHON.SecurityPackage;
+
+import com.example.ProjectHON.User_masterpackage.UserMaster;
+import com.example.ProjectHON.User_masterpackage.UserMasterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserMasterRepository userMasterRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        UserMaster user = userMasterRepository.findByEmail(email).orElse(null);
+        UserMaster user = userMasterRepository.findByUsername(username);
+        if(user!=null) {
+            return user;
+        }
+        throw new UsernameNotFoundException("User not found with username "+username);
+    }
+}
