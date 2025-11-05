09f31a77e75003f38829fffbb6d5349697761643
Muskan Shroti <shrotimuskan@gmail.com>
2025-11-05 21:52:16 +0530

    Add files via upload

---
+++ ProjectHON/src/main/java/com/example/ProjectHON/SecurityPackage/OAuthAuthenticationSuccessHandler.java
@@ -0,0 +1,82 @@
+package com.example.ProjectHON.SecurityPackage;
+
+import com.example.ProjectHON.User_masterpackage.UserMaster;
+import com.example.ProjectHON.User_masterpackage.UserMasterRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+
+@Component
+public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    UserMasterRepository userMasterRepository;
+
+
+    Logger logger = LoggerFactory.getLogger(OAuthAuthenticationSuccessHandler.class);
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication ) throws IOException, ServletException {
+        logger.info("OAuthAuthenticationSuccessHandler");
+
+        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+
+        //for printing in console.
+//       logger.info(user.getName());
+//
+//       user.getAttributes().forEach((key , value) ->{
+//           logger.info("{} => {}",key, value);
+//       });
+//
+//       logger.info(user.getAttributes().toString());
+
+        //saving data on db.
+
+       String email =  user.getAttribute("email").toString();
+        String name = user.getAttribute("name").toString();
+        String picture = user.getAttribute("picture").toString();
+
+        //creating a new obj of Usermaster.
+
+        UserMaster userMaster = new UserMaster();
+        userMaster.setUsername(name);
+        userMaster.setEmail(email);
+        userMaster.setProfilePhoto(picture.getBytes());
+//        userMaster.setUserId(UUID.randomUUID().node());
+        userMaster.setRoleList(List.of("ROLE_USER"));
+
+        HttpSession session=request.getSession();
+       UserMaster userMaster1 = userMasterRepository.findByEmail(email).orElse(null);
+
+       if(userMaster1 == null){
+           userMasterRepository.save(userMaster);
+           logger.info(user.getAttribute("name"));
+
+           session.setAttribute("userId",userMaster.getUserId());
+       }
+       else{
+           session.setAttribute("userId",userMaster1.getUserId());
+       }
+
+
+
+
+        new DefaultRedirectStrategy().sendRedirect(request,response,"/user/profile");
+//        new DefaultRedirectStrategy().sendRedirect(request,response,"user/profile");
+    }
+}
